6c8747bce5dde373579415462c29bdd998b17df0
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2023-10-30 17:25:30 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import '@material/web/ripple/ripple.js';
+import '@material/web/button/elevated-button.js';
+import '@material/web/button/filled-tonal-button.js';
---
+++ web-dev-server.config.js
@@ -0,0 +1,8 @@
+export default {
+    open: true,
+    watch: true,
+    appIndex: 'src/index.html',
+    nodeResolve: {
+        exportConditions: ['development'],
+    },
+};
