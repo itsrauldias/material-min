d5b56ab553c4ca77e6e63850b87fa9720fad8b0c
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2023-10-31 11:25:58 -0400

    231031-02

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import '@material/web/ripple/ripple.js';
-import '@material/web/button/elevated-button.js';
-import '@material/web/button/filled-tonal-button.js';
+import '@material/web/icon/icon.js'
+import '@material/web/button/filled-tonal-button';
+import '@material/web/button/filled-button.js';
+import '@material/web/button/text-button.js';
+import '@material/web/progress/circular-progress.js';
+import '@material/web/progress/linear-progress.js';
+import '@material/web/dialog/dialog.js';
+import '@material/web/radio/radio.js';
+import '@material/web/textfield/filled-text-field.js';
+import '@material/web/textfield/outlined-text-field.js';
