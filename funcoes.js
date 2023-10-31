806aec6bdeb2c2055848bba5a34f2e7fbb0eea82
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2023-10-31 11:22:55 -0400

    Updates

--- funcoes.js
+++
@@ -1,8 +0,0 @@
-function openDialog() {
-    let testdialog = document.getElementById('testdialog')
-    testdialog.show()
-}
-
-function changeTheme(theme) {
-    document.querySelector("#theme").href = "css/" + theme + "/theme.css";
-}
